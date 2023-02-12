817d88172dcc6dd8f457e7e7e07154fe9e6a029e
Björn Petersen <git@bjoernpetersen.net>
2023-02-12 17:48:20 +0100

    Implement createPlaylist()

--- lib/src/api/playlist.dart
+++ lib/src/api/playlist.dart
@@ -1,12 +1,37 @@
 import 'package:spotify_api/api.dart';
 import 'package:spotify_api/src/api/core.dart';
 import 'package:meta/meta.dart';
+import 'package:spotify_api/src/requests.dart';
 
 @immutable
 class SpotifyPlaylistApiImpl implements SpotifyPlaylistApi {
   final CoreApi core;
 
   SpotifyPlaylistApiImpl(this.core);
+
+  @override
+  Future<Playlist<Page<PlaylistTrack>>> createPlaylist({
+    required String userId,
+    required String name,
+    PlaylistVisibility? visibility,
+    String? description,
+  }) async {
+    final url = core.resolveUri('/users/$userId/playlists');
+
+    final response = await core.client.post(
+      url,
+      headers: await core.headers,
+      body: RequestBody.json(CreatePlaylist.withVisibility(
+        name: name,
+        visibility: visibility,
+        description: description,
+      )),
+    );
+
+    core.checkErrors(response);
+
+    return response.body.decodeJson(Playlist.fromJson);
+  }
 
   @override
   Future<Page<Playlist<PageRef<PlaylistTrack>>>>
--- lib/src/api_models/playlists/request.dart
+++ lib/src/api_models/playlists/request.dart
@@ -1,5 +1,6 @@
 import 'package:json_annotation/json_annotation.dart';
 import 'package:spotify_api/src/api_models/model.dart';
+import 'package:spotify_api/src/api_models/playlists/visibility.dart';
 
 part 'request.g.dart';
 
@@ -18,6 +19,16 @@ class CreatePlaylist implements RequestModel {
     this.isCollaborative,
     this.description,
   });
+
+  CreatePlaylist.withVisibility({
+    required this.name,
+    required PlaylistVisibility? visibility,
+    required this.description,
+  })  : isCollaborative = visibility == null
+            ? null
+            : visibility == PlaylistVisibility.collaborative,
+        isPublic =
+            visibility == null ? null : visibility == PlaylistVisibility.public;
 
   @override
   Map<String, dynamic> toJson() => _$CreatePlaylistToJson(this);
